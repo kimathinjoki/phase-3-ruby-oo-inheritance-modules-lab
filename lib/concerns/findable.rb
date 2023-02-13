c8b7037511edf1aa6fc8d5f83db48cf743c5f414
kimathinjoki <dfkimathinjoki@gmail.com>
2023-02-13 09:26:21 -0800

    Completed assignment

---
+++ lib/concerns/findable.rb
@@ -0,0 +1,14 @@
+
+
+module Findable
+
+    attr_accessor :name
+    def find_by_name(name)
+         @@all.detect{|a| a.name == name}
+    end
+
+    # def find_by_name(name)
+    #     @@ar.detect{|a| a.name == name}
+    # end
+
+end
